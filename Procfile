18e008652da6d9e7abc000a6ff52a4d4bd2200cb
Tosin Dairo <lade@tosins-mbp.home>
2019-12-02 06:39:47 +0000

    Update Dashboard

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: env FAKECHROOT_EXCLUDE_PATH=/tmp/app-initialized fakechroot fakeroot chroot /app/.root /usr/bin/R -f /app/run.R --gui-none --no-save 
--- run.R
+++ run.R
@@ -11,20 +11,23 @@ library(shiny)
 # When we run nginx we need to change R to point to the /app folder manually
 setwd('/app')
 
+port <- Sys.getenv('PORT')
+
 # Create a file at the path /tmp/app-initalized
 # This tells nginx that the app is running, and it can now forward traffic to the custom 'port' below
 # If this file is created, nginx will simply wait forever until the app says its running
-file.create("/tmp/app-initialized")
-port <- '/tmp/nginx.socket'
-attr(port, 'mask') <- strtoi("117", 8)
+#file.create("/tmp/app-initialized")
+#port <- '/tmp/nginx.socket'
+#attr(port, 'mask') <- strtoi("117", 8)
 
 
 # Runs the app in the working directory. By default this will search for an `app.R` file or 
 # a ui.R + server.R file combination
 # In this case the port is now the address to the nginx proxy
 rmarkdown::run(
-  'LAG_RUG_DASH_v0.2'
+  'Swapdash_v0.2.Rmd'
   , shiny_args = list(
-    port = port
+    host = '0.0.0.0',
+    port = as.numeric(port)
   )
 )
