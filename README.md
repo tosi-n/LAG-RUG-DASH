4ffa0cbf1d912fa23280e463affb6eb4584b6689
Tosin Dairo <lade@tosins-mbp.home>
2019-12-02 07:09:39 +0000

    Update Dashboard

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Open source Shiny Dashboard for Lagos R user Community.
 
 Presentation materials https://docs.google.com/presentation/d/16dy_5JV0SqDYkyzJLO1wCxfztkOFYziuYx_kpEnALY8/edit?usp=sharing
+
+Dashboard https://lag-rug-dash.herokuapp.com/#section-e-commerce
--- run.R
+++ run.R
@@ -13,13 +13,6 @@ setwd('/app')
 
 port <- Sys.getenv('PORT')
 
-# Create a file at the path /tmp/app-initalized
-# This tells nginx that the app is running, and it can now forward traffic to the custom 'port' below
-# If this file is created, nginx will simply wait forever until the app says its running
-#file.create("/tmp/app-initialized")
-#port <- '/tmp/nginx.socket'
-#attr(port, 'mask') <- strtoi("117", 8)
-
 
 # Runs the app in the working directory. By default this will search for an `app.R` file or 
 # a ui.R + server.R file combination
